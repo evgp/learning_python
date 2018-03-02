062196719ef8abbbbe46dee5081bffee1a33a116
bratikha <bratikha@gmail.com>
2018-03-03 09:05:53 +1200

    task completed

--- w4/w4_classfile.py
+++ w4/w4_classfile.py
@@ -2,31 +2,35 @@ import os
 from tempfile import gettempdir
 
 class File():
-    def __init__(self, path=None):
+    def __init__(self, path):
         self.path = path
-        with open(path, 'w') as f:
-            pass
-
+        try:
+            open(path, 'r')
+        except:
+            with open(path, 'w') as f:
+                pass
+            
 
     def __iter__(self):
-        with (self.path, 'r') as f:
-            return f
+        self.f = open(self.path, 'r')
+        return self.f
 
     def __next__(self):
-        pass
-
+        next(self.f)
+   
     def __add__(self, obj):
         with open(os.path.join(gettempdir(),self.path[-5:-4]+obj.path[-5:]), 'w') as f:
             with open(self.path, 'r') as f1:
                 r1 = f1.readlines()
             with open(obj.path, 'r') as f2:
                 r2 = f2.readlines()
-            f.writelines(r1+r2)       
+            f.writelines(r1+r2)
+        return File(os.path.join(gettempdir(),self.path[-5:-4]+obj.path[-5:]))       
 
     def __str__(self):
         return self.path
       
     def write(self, newline):
-        with open(self.path, 'w') as f:
+        with open(self.path, 'a') as f:
             f.write(newline)
 
