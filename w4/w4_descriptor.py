c51ac841f148ee8b9612eb27f04b38f782427f33
Evg P <evg.p@hotmail.com>
2018-03-05 17:10:59 +1200

    ok

--- w4/w4_descriptor.py
+++ w4/w4_descriptor.py
@@ -8,3 +8,14 @@ class Value:
 
     def __str__(self):
         return str(self.value)
+
+
+    #     class Value:
+    # def __init__(self):
+    #     self.amount = 0
+
+    # def __get__(self, obj, obj_type):
+    #     return self.amount
+
+    # def __set__(self, obj, value):
+    #     self.amount = value - value * obj.commission
