bbf4a8d2609cc51eda564f3416682c622cbe72d5
Evg P <evg.p@hotmail.com>
2018-03-12 10:30:26 +1200

    Preparetion metric client playground

--- w5/w5_server_multiprocess_socket.py
+++ w5/w5_server_multiprocess_socket.py
@@ -1,4 +1,7 @@
-import socket, threading, multiprocess
+import os
+import socket
+import threading
+import multiprocess
 
 
 def process_request(conn, addr):
@@ -9,13 +12,25 @@ def process_request(conn, addr):
             if not data:
                 break
             print(data.decode("utf8"))
-            
+
+
+def worker(sock):
+    while True:
+        conn, addr = sock.accept()  # system call to accept conn. 
+        print("pid", os.getpid())  # Every conn will be distributed by os
+        th = threading.Thread(target=process_request, args=(conn, addr))
+        th.start()
 
 with socket.socket() as sock:
     sock.bind(("", 10001))
     sock.listen()
-    while True:
-        conn, addr = sock.accept()
-        th = threading.Thread(target=process_request, args=(conn, addr))
 
-        th.start()
+    workers_count = 3
+    workers_list = [multiprocess.Process(target=worker, args=(sock,))
+                    for _ in range(workers_count)]
+
+    for w in workers_list:
+        w.start()
+
+    for w in workers_list:
+        w.join()  # wait until our process complete
