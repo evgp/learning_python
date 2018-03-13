09b08c3607945b7717340bc7026901037897ccc0
Evg P <6857762+evgp@users.noreply.github.com>
2018-03-13 17:01:55 +1200

    Tests passed
     python3 -m unittest test_client.py
    .....
    ----------------------------------------------------------------------
    Ran 5 tests in 0.001s
    
    OK

--- w5/metric-client/client.py
+++ w5/metric-client/client.py
@@ -8,18 +8,29 @@ class Client:
         self.addr = addr
         self.port = port
         try:
-            self.sock = socket.create_connection((self.addr, self.port))
+            self.sock = socket.create_connection((self.addr, self.port), self.timeout)
         except:
             raise ClientError()
 
     def __del__(self):
         self.sock.close
 
-    @staticmethod
-    def parse_get(got_string):
+    def put(self, key, value, timestamp=datetime.datetime.now().timestamp()):
+        self.timestamp = timestamp  # or datetime.datetime.now().timestamp()
         try:
+            data_send = f"put {key} {value} {self.timestamp}\n"
+            self.sock.sendall(data_send.encode("utf-8"))
+            # if self.sock.recv(1024) == "error\nwrong command\n\n":
+            #     raise ClientError()
+        except:
+            raise ClientError
+
+    def get(self, key):
+        try:
+            data_send = f"get {key}\n"
+            self.sock.sendall(data_send.encode("utf-8"))
             result = {}
-            srv_answer = got_string.decode("utf-8").splitlines()
+            srv_answer = self.sock.recv(1024).decode("utf-8").splitlines()
             if (srv_answer[0] != "ok"):
                 raise ClientError()
             for answ in srv_answer:
@@ -28,27 +39,6 @@ class Client:
                     result.setdefault(answ[0], [])
                     result[answ[0]].append((int(answ[2]), float(answ[1])))              
             return result
-        except:
-            raise ClientError()
-
-    def put(self, key, value, timestamp=datetime.datetime.now().timestamp()):
-        self.timestamp = timestamp  # or datetime.datetime.now().timestamp()
-        try:
-            data_send = f"put {key} {value} {self.timestamp}\n"
-            self.sock.sendall(data_send.encode("utf-8"))
-            if self.sock.recv(1024) == "error\nwrong command\n\n":
-                raise ClientError()
-        except:
-            raise ClientError
-
-    def get(self, key):
-        try:
-            data_send = f"get {key}\n"
-            self.sock.sendall(data_send.encode("utf-8"))
-            a = self.sock.recv(1024).decode("utf-8").splitlines()
-            if a[0] != "ok":
-                raise ClientError()
-            return self.parse_get(self.sock.recv(1024))
         except:
             raise ClientError()
 
