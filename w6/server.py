4d440e32dc502cd32968c47bd9bf884ffe54ee18
bratikha <bratikha@gmail.com>
2018-03-18 10:59:11 +1200

    Graded succesfully

--- w6/server.py
+++ w6/server.py
@@ -2,22 +2,17 @@ import asyncio
 from time import sleep
 
 metric={}
-host = "127.0.0.1"
-port = 8181
+
 
 class ClientServerProtocol(asyncio.Protocol):
     def __init__(self):
         self.err_msg = "error\nwrong command\n\n"
         self.ok_msg = "ok\n\n"
-        # self.metric = {}
 
     def connection_made(self, transport):
         self.transport = transport
 
     def process_data(self, data):
-        # self.transport.pause_reading()
-        # sleep(1)
-        # self.transport.resume_reading()
         commands = ("get", "put")
         try:
             if len(data.split()) > 2:
@@ -36,7 +31,6 @@ class ClientServerProtocol(asyncio.Protocol):
                     metric[key] = []
                 if (timestamp, value) not in metric[key]:
                     metric[key].append((timestamp, value))
-                print(metric)
             if client_cmd == "get":
                 if key == "*":
                     return metric
@@ -47,7 +41,6 @@ class ClientServerProtocol(asyncio.Protocol):
             return self.ok_msg
 
     def data_received(self, data):
-        print(data.decode())
         resp = self.process_data(data.decode())
         if isinstance(resp, dict):
             self.transport.write("ok\n".encode())
@@ -62,20 +55,20 @@ class ClientServerProtocol(asyncio.Protocol):
             self.transport.write(resp.encode())
 
 
-# def run_server(host, port):
-loop = asyncio.get_event_loop()
-coro = loop.create_server(
-    ClientServerProtocol,
-    host, port
-)
+def run_server(host, port):
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(
+        ClientServerProtocol,
+        host, port
+    )
 
-server = loop.run_until_complete(coro)
+    server = loop.run_until_complete(coro)
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    exit()
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
 
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
